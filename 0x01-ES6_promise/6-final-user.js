53ff9562f678b7b6a920fde13a6deedb5049c470
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-02 17:59:14 +0000

    task 7 errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,13 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => {
-      return result.map((res) => ({
+      result.map((res) => ({
         status: res.status,
-        value: res.status === 'fulfilled' ? res.value : res.reason,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
       }));
     });
 }
