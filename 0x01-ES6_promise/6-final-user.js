27538686f5bfe9aecf63356af1acbb9a0f9d7df6
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-02 17:06:34 +0000

    solve task number 7 errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,17 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((result) => {
-      result.map((res) => {
-        if (res.status === 'fulfilled') {
-          return {
-            status: 'fulfilled',
-            value: res.value,
-          };
-        }
-        return {
-          status: 'rejected',
-          value: String(res.reason),
-        };
-      });
+      result.map((res) => ({
+        status: res.status,
+        value: res.status === 'fulfilled' ? res.value : String(res.reason),
+      }));
     });
 }
