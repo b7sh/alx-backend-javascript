586326676472c233a97088d94abbc7286513b15f
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-02 18:01:10 +0000

    solve seventh task errors

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,10 +4,10 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((result) => {
+    .then((result) => (
       result.map((res) => ({
         status: res.status,
         value: res.status === 'fulfilled' ? res.value : String(res.reason),
-      }));
-    });
+      }))
+    ));
 }
