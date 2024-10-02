5d212889a81b300c12e1121aa8e5c3ad8e35e5cd
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-02 14:12:11 +0000

    solve the task number 7 6. Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,20 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((result) => {
+      result.map((res) => {
+        if (res.status === 'fulfilled') {
+          return {
+            status: 'fulfilled',
+            value: res.value,
+          };
+        }
+        return {
+          status: 'rejected',
+          value: res.reason,
+        };
+      });
+    });
+}
