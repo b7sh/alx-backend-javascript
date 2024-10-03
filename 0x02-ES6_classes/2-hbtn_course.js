0edb94cb07468e1adb2e35bdd87089c0b5fd354d
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-03 13:24:02 +0000

    solve the third task '2. A Course, Getters, and Setters'

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,44 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    this._name = name;
+    this._length = length;
+    this._students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(length) {
+    if (typeof length !== 'number') {
+      throw new TypeError('Length must be a number');
+    } else {
+      this._length = length;
+    }
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(students) {
+    const isString = students.every((item) => typeof item === 'string');
+    if (Array.isArray(students) && isString) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of Strings');
+    }
+  }
+}
