0d1399c3c1221cda03d1c6e5645355f56e09607e
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-07 14:03:34 +0000

    solve the fifth task '4. Combine'

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(listOfObjects, city, newGrades) {
+  return listOfObjects
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const studentGrade = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : 'N/A',
+      };
+    });
+}
