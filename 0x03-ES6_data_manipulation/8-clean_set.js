d6385aa3052dae90b340f98949f743e521c157db
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-07 16:22:52 +0000

    solve the ninth task '8. Clean set'

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,15 @@
+export default function cleanSet(set, startString) {
+  let result = '';
+
+  if (startString.length > 0) {
+    for (const value of set) {
+      if (value.startsWith(startString)) {
+        result += `${value.replace(startString, '')}-`;
+      }
+    }
+  }
+  if (result.length > 0) {
+    result = result.slice(0, -1);
+  }
+  return result;
+}
