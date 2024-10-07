417e9e1e408831ae3dad08665e954d54e76579c7
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-07 16:52:52 +0000

    solve the nint task errors again third time

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,15 +1,14 @@
 export default function cleanSet(set, startString) {
   let result = '';
 
-  if (typeof startString === 'string' && startString.length > 0) {
+  if (typeof startString === 'string'
+      && startString.length > 0
+      && typeof set === 'object') {
     for (const value of set) {
       if (value.startsWith(startString)) {
         result += `${value.replace(startString, '')}-`;
       }
     }
-  }
-  else {
-    return '';
   }
   if (result.length > 0) {
     result = result.slice(0, -1);
