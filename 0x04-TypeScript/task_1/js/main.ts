6d3ecfc9a84bba5fe6a1098149cb559388d7b9de
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-09 17:40:14 +0000

    solve the fifth task '4. Writing a class'

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,28 @@ export function printTeacher(firstName: string, lastName: string): string {
 export interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
+
+export class StudentClass implements IStudentClass{
+  private _firstName!: string;
+  private _lasstName!: string;
+
+  constructor(firstName: string, lastName: string) {
+    this._firstName = firstName;
+    this._lasstName = lastName;
+  }
+  workOnHomework() {
+    return 'Currently working';
+  }
+  displayName() {
+    return this._firstName;
+  }
+}
+
+export interface IStudentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+export interface IStudentClassConstructor {
+  new (firstName: string, lasstName: string): IStudentClass;
+}
