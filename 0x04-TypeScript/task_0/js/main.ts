74b028a2f29411f087bd37ead99b16230fe8bc8e
b7sh <mohammedabdelbasit500@gmail.com>
2024-10-09 17:14:28 +0000

    solve the first task '0. Creating an interface for a student'

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,89 @@
+export interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const firstStudent: Student = {
+  firstName: 'boshy',
+  lastName: 'abdalla',
+  age: 23,
+  location: "Burydah, saudi arabia",
+}
+
+const secondStudent: Student = {
+  firstName: 'fageer',
+  lastName: 'saeed',
+  age: 24,
+  location: "Riyadh, saudi arabia",
+}
+
+const studentsList: Array<Student>;
+
+studentsList = [firstStudent, secondStudent];
+
+const styleSheet = `
+  html {
+    margin: 0;
+    height: 100%;
+  }
+  body {
+    box-sizing: border-box;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 80%;
+    margin: 10%;
+  }
+  table {
+    border-collapse: collapse;
+  }
+  thead {
+    font-weight: bold;
+  }
+  td {
+    padding: 10px;
+    border: 1px solid gray;
+    cursor: pointer;
+  }
+  td:hover {
+    background: gainsboro;
+  }
+
+  td:nth-child(1) {
+    text-align: center;
+  }
+`;
+
+/**
+ * Displays information about students in a table.
+ * @param students The list of students to display.
+ */
+
+export const displayStudents = (students: Array<Student>): void => {
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const headRow = document.createElement('tr');
+  const tableBody = document.createElement('tbody');
+  headRow.insertAdjacentHTML('beforeend', '<td>FirstName</td');
+  headRow.insertAdjacentHTML('beforeend', '<td>Location</td');
+  tableHead.insertAdjacentElement('beforeend', headRow);
+
+  for (const student of students) {
+    const bodyRow = document.createElement('tr');
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.firstName}</td>`);
+    bodyRow.insertAdjacentHTML('beforeend', `<td>${student.location}</td>`);
+    tableBody.insertAdjacentElement('beforeend', bodyRow);
+  }
+
+  table.insertAdjacentElement('beforeend', tableHead);
+  table.insertAdjacentElement('beforeend', tableBody);
+  document.body.insertAdjacentElement('beforeend', table);
+};
+
+displayStudents(studentsList);
+const styleSheetElement = document.createElement('style');
+styleSheetElement.innerHTML = styleSheet;
+document.head.insertAdjacentElement('beforeend', styleSheetElement);
+document.title = 'Task 0';
